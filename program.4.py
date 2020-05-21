a0789bf64ff26ff81266c8db19229db9cc915e81
prakharima singh <65663329+PrakharimaSingh@users.noreply.github.com>
2020-05-21 21:27:38 +0530

    Create program.4.py

---
+++ program.4.py
@@ -0,0 +1,9 @@
+number = int(input("Enter the value of N : "))
+
+print("\n")
+
+for i in range(number):
+    print((number-i) * '*' + (i * 2) * ' ' + (number - i) * '*')
+for i in range(number):
+    print((i+1) * '*' + (number - i - 1) * ' ' + (number - i-1) * ' ' + (i+1) * '*')
+© 2020 GitHub, Inc.
